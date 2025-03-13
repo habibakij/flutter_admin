eb9cdecd0329002e40625b35cb8871d8002fa30e
akijmia <akij.mia@technonext.com>
2025-03-13 22:40:21 +0600

    local server post add

--- lib/ui/new_place.dart
+++ lib/ui/new_place.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
 class AddNewPlaceScreen extends StatefulWidget {
   const AddNewPlaceScreen({super.key});
@@ -21,7 +22,8 @@ class _AddNewPlaceScreenState extends State<AddNewPlaceScreen> {
   TextEditingController locationController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
 
-  Future<void> _dataPostRequest() async {
+  Future<void> _dataPostRequest(
+      {String title = '', String description = ''}) async {
     if (selectedImages.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Please select images to upload")),
@@ -29,10 +31,10 @@ class _AddNewPlaceScreenState extends State<AddNewPlaceScreen> {
       return;
     }
 
-    var uri = Uri.parse("http://localhost:3000/add_new_place");
+    final uri = Uri.parse("http://10.0.2.2:3000/add_new_place");
     var request = http.MultipartRequest("POST", uri);
-    request.fields["placeTitle"] = "Beautiful Beach";
-    request.fields["placeDes"] = "A wonderful place to visit";
+    request.fields["placeTitle"] = title;
+    request.fields["placeDes"] = description;
     for (var image in selectedImages) {
       var file = await http.MultipartFile.fromPath("placeImage", image.path);
       request.files.add(file);
@@ -43,7 +45,7 @@ class _AddNewPlaceScreenState extends State<AddNewPlaceScreen> {
       var responseBody = await response.stream.bytesToString();
       var jsonResponse = jsonDecode(responseBody);
 
-      if (response.statusCode == 201) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         Get.snackbar("INFO", "Images uploaded successfully! ✅");
         log("Response: $jsonResponse");
       } else {
@@ -61,98 +63,103 @@ class _AddNewPlaceScreenState extends State<AddNewPlaceScreen> {
       padding: const EdgeInsets.all(16.0),
       child: Form(
         key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 60.0,
-              width: double.infinity,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color: Color(0xFF144CA1),
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-              ),
-              child: const Text(
-                "Add New Place",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(height: 24),
-            _textInputWidget(
-                controller: titleController, labelText: "Place Title"),
-            const SizedBox(height: 16),
-            _textInputWidget(
-                controller: locationController, labelText: "Place Location"),
-            const SizedBox(height: 16),
-            _textInputWidget(
-                controller: descriptionController,
-                labelText: "Place Details",
-                maxLine: 3),
-            const SizedBox(height: 16),
-            Obx(() => selectedImages.isEmpty
-                ? const Padding(
-                    padding: EdgeInsets.only(left: 8.0),
-                    child: Text("No images selected",
-                        style: TextStyle(fontSize: 18)),
-                  )
-                : Wrap(
-                    spacing: 8,
-                    runSpacing: 8,
-                    children: selectedImages
-                        .map((image) => ClipRRect(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(24.0)),
-                            child: Image.network(image.path, height: 100)))
-                        .toList(),
-                  )),
-            const SizedBox(height: 24),
-            SizedBox(
-              width: 300.0,
-              child: ElevatedButton(
-                onPressed: () async {
-                  final pickedFiles = await picker.pickMultiImage();
-                  if (pickedFiles.isNotEmpty) {
-                    selectedImages.addAll(pickedFiles);
-                  }
-                },
-                style: ButtonStyle(
-                  foregroundColor:
-                      WidgetStateProperty.all(const Color(0xFF144CA1)),
-                  padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
-                  textStyle:
-                      WidgetStateProperty.all(const TextStyle(fontSize: 16)),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 60.0,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF144CA1),
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
                 ),
                 child: const Text(
-                  "Select Images",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  "Add New Place",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-            ),
-            const SizedBox(height: 50.0),
-            SizedBox(
-              width: 400.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _dataPostRequest();
-                    Get.snackbar("Success", "Profile updated successfully!");
-                  }
-                },
-                style: ButtonStyle(
+              const SizedBox(height: 24),
+              _textInputWidget(
+                  controller: titleController, labelText: "Place Title"),
+              const SizedBox(height: 16),
+              _textInputWidget(
+                  controller: locationController, labelText: "Place Location"),
+              const SizedBox(height: 16),
+              _textInputWidget(
+                  controller: descriptionController,
+                  labelText: "Place Details",
+                  maxLine: 3),
+              const SizedBox(height: 16),
+              Obx(() => selectedImages.isEmpty
+                  ? const Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text("No images selected",
+                          style: TextStyle(fontSize: 18)),
+                    )
+                  : Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: selectedImages
+                          .map((image) => ClipRRect(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(24.0)),
+                              child: Image.file(File(image.path), height: 100)))
+                          .toList(),
+                    )),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: 300.0,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final pickedFiles = await picker.pickMultiImage();
+                    if (pickedFiles.isNotEmpty) {
+                      selectedImages.addAll(pickedFiles);
+                    }
+                  },
+                  style: ButtonStyle(
+                    foregroundColor:
+                        WidgetStateProperty.all(const Color(0xFF144CA1)),
+                    padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
+                    textStyle:
+                        WidgetStateProperty.all(const TextStyle(fontSize: 16)),
+                  ),
+                  child: const Text(
+                    "Select Images",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50.0),
+              SizedBox(
+                width: 400.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _dataPostRequest(
+                        title: titleController.text.toString(),
+                        description: descriptionController.text.toString(),
+                      );
+                    }
+                  },
+                  style: ButtonStyle(
                     foregroundColor:
                         WidgetStateProperty.all(const Color(0xFFFFFFFF)),
                     backgroundColor:
                         WidgetStateProperty.all(const Color(0xFF144CA1)),
                     padding: WidgetStateProperty.all(const EdgeInsets.all(20)),
                     textStyle:
-                        WidgetStateProperty.all(const TextStyle(fontSize: 20))),
-                child: const Text("Submit"),
+                        WidgetStateProperty.all(const TextStyle(fontSize: 20)),
+                  ),
+                  child: const Text("Submit"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
